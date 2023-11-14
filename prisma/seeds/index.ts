6c0c172c956d79dde7cf38bb41a91388453a7bdd
viandwi24 <viandwi24@pm.me>
2023-11-14 16:21:09 +0700

    🎉 feat: initialize project

---
+++ prisma/seeds/index.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+import { PrismaClient } from "@prisma/client"
+import dotenv from "dotenv"
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env'),
+});
+const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    }
+  }
+});
+
+// TODO: Add your seed here
+// await db.law.create({
+//   data: {
+//     title: "Kitab Hukum Perdata",
+//     description: "Kitab Hukum Perdata adalah kitab hukum yang mengatur mengenai hukum perdata",
+//     LawBab: {
+//       create: {
+//         name: "Bab I",
+//         LawData: {
+//           create: [
+//             { pasal: '1', content: 'isi dari pasal 1' },
+//             { pasal: '2', content: 'isi dari pasal 2' },
+//             { pasal: '3', content: 'isi dari pasal 3' },
+//           ]
+//         }
+//       }
+//     }
+//   },
+// })
+
+console.log("Seeding completed");
